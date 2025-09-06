70620222968c9842c83a2f856923b41e4ef31fa1
Stephen Carico <scarico@pax8.com>
2025-09-05 19:30:48 -0700

    feat: get and post todos

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 /// <reference types="node" />
 // app.ts
-import express, { Request, Response } from "express";
+import express from "express";
 const app = express();
 const PORT = 3000;
 
@@ -8,11 +8,29 @@ const PORT = 3000;
 app.use(express.json());
 
 // Sample route
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+let todos: string[] = [
+  'Buy groceries',
+  'Trim my beard',
+  'Wash my car',
+  'Clean my room',
+];
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const todo = req.body;
+  todos.push(todo);
+  res.status(201).json(todo);
 });
